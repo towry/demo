117440201d48d4d2a72c88748ef725c1d266f338
towry <tovvry@gmail.com>
2021-08-26 09:05:26 +0000

    Deploying to gh-pages from  @ ef26599004a347ff992c5515fb7704ac8508d440 🚀

--- js/chunk-25acd7c5.0bff142a.js
+++ js/chunk-25acd7c5.0bff142a.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-25acd7c5"],{"445f":function(t,n,a){},c187:function(t,n,a){"use strict";var c=a("445f"),s=a.n(c);s.a},e29d:function(t,n,a){"use strict";a.r(n);var c=function(){var t=this,n=t.$createElement,a=t._self._c||n;return a("v-card",{staticClass:"mx-auto g-mt-40 g-px-30 g-py-20",attrs:{"max-width":"400"}},[a("h1",[t._v(" Not Found "),a("router-link",{staticClass:"NotFound__back-link",attrs:{to:"/"}},[t._v("Home")])],1)])},s=[],o={},u=o,e=(a("c187"),a("2877")),r=a("6544"),i=a.n(r),l=a("b0af"),d=Object(e["a"])(u,c,s,!1,null,null,null);n["default"]=d.exports;i()(d,{VCard:l["a"]})}}]);
-//# sourceMappingURL=chunk-25acd7c5.76f52c63.js.map
+//# sourceMappingURL=chunk-25acd7c5.0bff142a.js.map
